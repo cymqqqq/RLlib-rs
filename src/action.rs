764285ecdd0a917d934b8cbc9b238bbcb75e2d70
Jack Choi <648074155@qq.com>
2021-07-18 10:14:09 +0800

    Update action.rs

--- src/action.rs
+++ src/action.rs
@@ -37,7 +37,96 @@ mod rllib {
             self.action_id != other.action_id
         }
     } 
-    
+    impl ActionTrait<i32> for Action<i32> {
+        fn push_back(&mut self, value: i32) {
+            self.values.push(value);
+        }
+        fn get_entry(&mut self, i: usize) -> i32 {
+            return self.values[i];
+        }
+        fn dim(&self) -> i32 {
+            return self.values.len() as i32;
+        }
+        fn update(&mut self, i: usize, value: i32) {
+            self.values[i] = value;
+        }
+        fn id(&self) -> i32 {
+            return self.action_id;
+        }
+
+    }
+    impl ActionTrait<f32> for Action<f32> {
+        fn push_back(&mut self, value: f32) {
+            self.values.push(value);
+        }
+        fn get_entry(&mut self, i: usize) -> f32 {
+            return self.values[i];
+        }
+        fn dim(&self) -> i32 {
+            return self.values.len() as i32;
+        }
+        fn update(&mut self, i: usize, value: f32) {
+            self.values[i] = value;
+        }
+        fn id(&self) -> i32 {
+            return self.action_id;
+        }
+
+    }
+    impl ActionTrait<f64> for Action<f64> {
+        fn push_back(&mut self, value: f64) {
+            self.values.push(value);
+        }
+        fn get_entry(&mut self, i: usize) -> f64 {
+            return self.values[i];
+        }
+        fn dim(&self) -> i32 {
+            return self.values.len() as i32;
+        }
+        fn update(&mut self, i: usize, value: f64) {
+            self.values[i] = value;
+        }
+        fn id(&self) -> i32 {
+            return self.action_id;
+        }
+
+    }
+    impl ActionTrait<String> for Action<String> {
+        fn push_back(&mut self, value: String) {
+            self.values.push(value);
+        }
+        fn get_entry(&mut self, i: usize) -> String {
+            return self.values[i].clone();
+        }
+        fn dim(&self) -> i32 {
+            return self.values.len() as i32;
+        }
+        fn update(&mut self, i: usize, value: String) {
+            self.values[i] = value;
+        }
+        fn id(&self) -> i32 {
+            return self.action_id;
+        }
+
+    }
+    impl ActionTrait<char> for Action<char> {
+        fn push_back(&mut self, value: char) {
+            self.values.push(value);
+        }
+        fn get_entry(&mut self, i: usize) -> char {
+            return self.values[i];
+        }
+        fn dim(&self) -> i32 {
+            return self.values.len() as i32;
+        }
+        fn update(&mut self, i: usize, value: char) {
+            self.values[i] = value;
+        }
+        fn id(&self) -> i32 {
+            return self.action_id;
+        }
+
+    }
 
 
     
