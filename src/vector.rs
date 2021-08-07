5ff89697a4ba2537f844c95a10b87021297a7f0c
Jack Choi <648074155@qq.com>
2021-08-07 11:43:12 +0800

    Update vector.rs

--- src/vector.rs
+++ src/vector.rs
@@ -1,20 +1,46 @@
 mod rllib {
-  pub enum VectorType {
+    pub struct Vector<T>(T);
+    
+    pub enum VectorType {
         BASE_VECTOR,
         DENSE_VECTOR,
         SPARSE_VECTOR,
     }
+    /*
     pub struct Vector {
-        vectorType: VectorType,
+        vectortype: VectorType,
     }
+    */
     pub trait VectorTrait<T> {
-        fn dimmension() -> i32;
-        fn empty() -> bool;
-        fn max_norm() -> T;
-        fn l1_norm() -> T;
-        fn l2_norm() -> T;
-        fn sum() -> T;
-        fn get_value() -> Box<T>;
-        
+        fn dimension(&self) -> i32;
+        fn empty(&self) -> bool;
+        fn max_norm(&self) -> T;
+        fn l1_norm(&self) -> T;
+        fn l2_norm(&self) -> T;
+        fn sum(&self) -> T;
+        fn get_values(&self) -> Box<T>;
+        fn get_entry(&self, idx: usize) -> T;
+        fn dot(&mut self, other: Box<Vector<T>>) -> T;
+        fn clear(&mut self);
+        fn set_entry(&mut self, idx: i32, val: T);
+        fn remove_entry(&mut self, idx: i32);
+        fn addToself(&mut self, val: T) -> Box<Vector<T>>;
+        //fn addToself(&mut self, factor: T, other: Box<Vector<T>>) -> Box<Vector<T>>;
+        //fn addToself(&mut self, other: Box<Vector<T>>) -> Box<Vector<T>>;
+        fn substractToself(&mut self, other: Box<Vector<T>>) -> Box<Vector<T>>;
+        fn mapMultiplyToself(&mut self, factor: T) -> Box<Vector<T>>;
+        fn set(&mut self, other: Box<Vector<T>>) -> Box<Vector<T>>;
+        //fn set(&mut self, other: Box<Vector<T>>, offset: i32) -> Box<Vector<T>>;
+        //fn set(&mut self, val: T) -> Box<Vector<T>>;
+        //deep copy of vector
+        fn copy(&mut self) -> Box<Vector<T>>;
+        fn newInstance(&mut self, dim: i32) -> Box<Vector<T>>;
+        //storage management
+        fn persist(&mut self, f: String);
+        fn resurrect(&mut self, f: String);
+    }
+    pub struct DenseVector<T> {
+        capacity: i32,
+        data: Box<T>,
     }
 }
